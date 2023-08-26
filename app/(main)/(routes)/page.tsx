3888c21497c701955393c173460f7e6b4551d217
moretime-dev <moretimeproductions@googlemail.com>
2023-08-26 17:30:43 +0200

    - added dark mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,14 @@
 import { UserButton } from "@clerk/nextjs";
-
+import { ModeToggle } from "@/components/mode-toggle";
 export default function Home() {
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
+    <section>
+      <div className="bg-slate-100 w-12 p-2">
+        <UserButton afterSignOutUrl="/" />
+      </div>
+      <div className="w-12 p-2">
+        <ModeToggle />
+      </div>
+    </section>
   );
 }
